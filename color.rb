0f6abcf555514b8cf0d3d6690ed7687376743161
steven huynh <steven.h@live.com>
2020-10-29 17:03:50 -0700

    added module Color

---
+++ color.rb
@@ -0,0 +1,12 @@
+module Color
+  def generate_random_color
+    case rand(1..6)
+    when 1 then 'red'
+    when 2 then 'orange'
+    when 3 then 'yellow'
+    when 4 then 'green'
+    when 5 then 'blue'
+    when 6 then 'purple'
+    end
+  end
+end
