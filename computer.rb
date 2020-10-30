1b8246053bde3bc2b02eeace00406d3b9c6ecb99
steven huynh <steven.h@live.com>
2020-10-29 18:22:40 -0700

    added check_guess method

--- computer.rb
+++ computer.rb
@@ -8,10 +8,22 @@ class Computer
     @code = random_color_array
   end
 
+  def check_guess(guess_array)
+    response = []
+    guess_array.each_with_index do |color, idx|
+      if color == code[idx]
+        response.push 'exactly correct'
+      elsif code.include? color
+        response.push 'correct color'
+      end
+    end
+    response
+  end
+
   private
 
   def random_color_array
-    color_array = Array.new(4)
+    color_array = Array.new 4
     color_array.each_index do |idx|
       color_not_exist = false
       until color_not_exist
@@ -25,6 +37,3 @@ class Computer
     color_array
   end
 end
-
-c = Computer.new
-p c.code
