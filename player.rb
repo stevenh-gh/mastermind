fffdf0093a054b6caaa2b461806dbf28eb701c91
steven huynh <steven.h@live.com>
2020-10-31 14:51:08 -0700

    added make_code method and also code instance var

--- player.rb
+++ player.rb
@@ -2,10 +2,27 @@ require_relative 'color'
 
 class Player
   include Color
-  attr_reader :name
+  attr_reader :name, :code
 
   def initialize(name)
     @name = name
+  end
+
+  def make_code
+    @code = []
+    4.times do |num|
+      print_color_list
+      legal_input = false
+      until legal_input
+        print "Enter color #{num + 1}: "
+        color = gets.chomp.downcase
+        if COLOR_LIST.include? color
+          @code.push color
+          legal_input = true
+        end
+      end
+    end
+    @code
   end
 
   def make_guess
@@ -14,9 +31,9 @@ class Player
       legal_input = false
       until legal_input
         print "Enter guess #{num + 1}: "
-        input = gets.chomp
+        input = gets.chomp.downcase
         if COLOR_LIST.include? input
-          guess.push(input)
+          guess.push input
           legal_input = true
         end
       end
