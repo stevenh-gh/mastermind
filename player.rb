332978abb3d6202c191be3952693b370d1f99b56
steven huynh <steven.h@live.com>
2020-10-30 17:28:16 -0700

    added make_guess method

--- player.rb
+++ player.rb
@@ -4,4 +4,13 @@ class Player
   def initialize(name)
     @name = name
   end
+
+  def make_guess
+    guess = []
+    4.times do |num|
+      print "Enter guess #{num}: "
+      guess.push(gets.chomp)
+    end
+    guess
+  end
 end
