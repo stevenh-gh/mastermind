7c95dc1f3654838a5a7aac7c60bd2032f1a1f928
steven huynh <steven.h@live.com>
2020-10-30 18:41:32 -0700

    added methods declare_win, win_condition, and print_color_list. added more to the main gameplay loop

--- game.rb
+++ game.rb
@@ -1,8 +1,11 @@
 require_relative 'player'
 require_relative 'computer'
+require_relative 'color'
 
 class Game
+  include Color
   attr_reader :player, :computer
+
   def initialize
     print 'Enter player name: '
     @player = Player.new gets.chomp
@@ -11,11 +14,33 @@ class Game
 
   def gameplay
     12.times do |num|
+      print_color_list
       puts "Round #{num + 1}:"
-      if computer.check_guess(player.make_guess)
-        win
+      guess = player.make_guess
+      response = computer.check_guess(guess)
+      print "You guessed: #{guess}\n"
+      print "Response: #{response}\n" unless response == win_condition
+      if response == win_condition
+        declare_win
         break
       end
+      print "#{player.name} has failed to decrypt the secret code..." if num == 11
     end
+  end
+
+  private
+
+  def print_color_list
+    print 'The current list of colors are: '
+    COLOR_LIST.each { |color| print "#{color} " }
+    puts
+  end
+
+  def win_condition
+    win_condition = Array.new 4, 'exactly correct'
+  end
+
+  def declare_win
+    print "#{player.name} has guessed the secret code correctly!"
   end
 end
