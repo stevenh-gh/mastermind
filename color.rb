25ffffca24125add283c59bc7a228e06558ca587
steven huynh <steven.h@live.com>
2020-10-30 17:31:22 -0700

    added color list and refactored random color method

--- color.rb
+++ color.rb
@@ -1,14 +1,16 @@
 module Color
   private
 
+  COLOR_LIST = %w[red orange yellow green blue purple].freeze
+
   def generate_random_color
     case rand(1..6)
-    when 1 then 'red'
-    when 2 then 'orange'
-    when 3 then 'yellow'
-    when 4 then 'green'
-    when 5 then 'blue'
-    when 6 then 'purple'
+    when 1 then COLOR_LIST[0]
+    when 2 then COLOR_LIST[1]
+    when 3 then COLOR_LIST[2]
+    when 4 then COLOR_LIST[3]
+    when 5 then COLOR_LIST[4]
+    when 6 then COLOR_LIST[5]
     end
   end
 end
