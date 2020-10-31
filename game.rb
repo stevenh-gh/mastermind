7f7dc6b512e032ceaabd52c9f19234e7b6f5e35d
steven huynh <steven.h@live.com>
2020-10-30 17:57:34 -0700

    added gameplay method

--- game.rb
+++ game.rb
@@ -2,9 +2,20 @@ require_relative 'player'
 require_relative 'computer'
 
 class Game
+  attr_reader :player, :computer
   def initialize
     print 'Enter player name: '
-    player = Player.new gets.chomp
-    computer = Computer.new
+    @player = Player.new gets.chomp
+    @computer = Computer.new
+  end
+
+  def gameplay
+    12.times do |num|
+      puts "Round #{num + 1}:"
+      if computer.check_guess(player.make_guess)
+        win
+        break
+      end
+    end
   end
 end
